f6d0dfe2c08fdb8fd6219df7821e51574d5ee7df
Jeremy Adams <jeremy.adams@ga4gh.org>
2022-05-12 16:08:46 -0400

    mock mvc and docker testing for mint endpoint

--- src/test/java/org/ga4gh/starterkit/passport/broker/controller/MintTest.java
+++ src/test/java/org/ga4gh/starterkit/passport/broker/controller/MintTest.java
@@ -11,16 +11,27 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import testutils.TestJwtVerifier;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -41,7 +52,42 @@ public class MintTest extends AbstractTestNGSpringContextTests {
     @DataProvider(name = "mintCases")
     public Object[][] mintCases() {
         return new Object[][] {
-            {}
+            {
+                "85ff5a54-48b9-4294-a91d-2be50bd2a77d",
+                "jeremy.adams@ga4gh.org",
+                "Jeremy",
+                "Adams",
+                new ArrayList<String>() {{
+                    add("670cc2e7-9a9c-4273-9334-beb40d364e5c");
+                    add("39e6e359-e8da-4193-92cc-2eed421fe729");
+                }},
+                status().isOk(),
+                true
+            },
+            {
+                "00000000-0000-0000-0000-000000000000",
+                "nobody@ga4gh.org",
+                "No",
+                "Body",
+                new ArrayList<String>() {{
+                    add("670cc2e7-9a9c-4273-9334-beb40d364e5c");
+                    add("39e6e359-e8da-4193-92cc-2eed421fe729");
+                }},
+                status().isBadRequest(),
+                false
+            },
+            {
+                "46d40e07-8d51-4e4d-a93a-e3438271172b",
+                "emre.ferhatoglu@ga4gh.org",
+                "Emre",
+                "Ferhatoglu",
+                new ArrayList<String>() {{
+                    add("670cc2e7-9a9c-4273-9334-beb40d364e5c");
+                    add("39e6e359-e8da-4193-92cc-2eed421fe729");
+                }},
+                status().isBadRequest(),
+                false
+            }
         };
     }
 
@@ -58,22 +104,20 @@ public class MintTest extends AbstractTestNGSpringContextTests {
         requestBody.setResearcherLastName(researcherLastName);
         requestBody.setRequestedVisas(requestedVisas);
 
+        ObjectMapper mapper = new ObjectMapper();
 
         MvcResult result = mockMvc.perform(
             post("/admin/ga4gh/passport/v1/mint")
-            .content("")
+            .content(mapper.writeValueAsString(requestBody))
             .header("Content-Type", "application/json")
         )
             .andExpect(expStatus)
             .andReturn();
         
         if (expSuccess) {
-            String responseBody = result.getResponse().getContentAsString();
-            ObjectMapper mapper = new ObjectMapper();
-            System.out.println("***");
-            System.out.println(responseBody);
-            System.out.println("***");
-            Assert.assertEquals("a", "b");
+            // verify the JWT as the passport level
+            String rawPassportJwt = result.getResponse().getContentAsString();
+            TestJwtVerifier.verify(rawPassportJwt);
         }
     }
 }
